aef29faf5cb2942be0d6ca070e6c79900102fc69
Elly Post <epost@luc.edu>
2017-11-16 19:53:04 -0600

    Started add JS

---
+++ js/order.js
@@ -0,0 +1,7 @@
+function toggleField(elementId) {
+    $("#" + elementId).removeClass("hidden");
+
+    // let ele = window.document.getElementById(elementId);
+    // ele.style.display = "block !important;";
+
+}
