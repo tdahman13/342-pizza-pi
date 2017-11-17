cba1a9b7d505f5ddebcb3bd28d7d995b4db07a15
Elly Post <epost@luc.edu>
2017-11-16 20:56:05 -0600

    Added JS integration

--- js/order.js
+++ js/order.js
@@ -1,7 +1,132 @@
-function toggleField(elementId) {
-    $("#" + elementId).removeClass("hidden");
+"use strict";
+
+// function toggleField(elementId) {
+//     $("#" + elementId).removeClass("hidden");
 
     // let ele = window.document.getElementById(elementId);
     // ele.style.display = "block !important;";
 
+// }
+
+// document.getElementById("crust-choice")
+//     .addEventListener('change', function(){
+//         console.log(document.getElementById("pizza-size-container"));
+//     });
+
+$(document).ready(function($){
+    let pizzaOrder = getOrder();
+
+
+
+    $("#crust-choice").on("change", function(e){
+        $("#pizza-size-container").removeClass("hidden");
+    });
+
+    $("#pizza-size").on("change", function(e){
+        $(".toppings-container").removeClass("hidden");
+    });
+
+    $(".pizza-updater").on("change", function(e){
+        let fieldName = $(this).attr('name');
+        pizzaOrder[fieldName] = $(this).val();
+        saveOrder(pizzaOrder);
+    });
+});
+
+function createOrder() {
+    return {
+        crust: null,
+        size: null,
+        toppingsMeat: [],
+        toppingsMisc: []
+    };
 }
+
+function saveOrder(pizzaOrder) {
+    localStorage.pizza_order = JSON.stringify(pizzaOrder);
+}
+
+function getOrder() {
+    return (localStorage['pizza_order'])
+        ? JSON.parse(localStorage['pizza_order'])
+        : createOrder();
+}
+
+// function sayHello() {
+//     alert("Hello!");
+// }
+//
+// sayHello();
+
+// let sayHello = function(name) {
+//     alert("hello " + name);
+// };
+//
+// sayHello(sayHello);
+
+// let sum = (a, b) => {return a + b;}
+
+
+// let donnaAge = 23;
+// let donnaName = "Donna";
+// let favColor = "brown";
+//
+// let donna = {
+//     age: 23,
+//     name: "Donna",
+//     favColor: "brown",
+// };
+//
+// console.log(donna);
+//
+//
+// let i;
+// for (i = 0; i < 10; i++) {
+//
+//     console.log("i is : " + i);
+// }
+// console.log("Done iteration, i is: " + i);
+
+//
+// let obj1 = {};
+// let obj2 = {};
+// let obj3 = {};
+// let obj4 = {};
+// let obj5 = {};
+//
+// let myObjects = [
+//     28,
+//     13,
+//     "hello",
+//     'c',
+//     12.3,
+//     -18,
+//     {
+//         age: 23,
+//         name: "Donna",
+//         favColor: "brown"
+//     }
+// ];
+//
+
+// let donna = {
+//     name: "Donna",
+//     favSongGenre: 'jazz'
+// };
+
+// localStorage['donna'] = JSON.stringify(donna);
+//
+//
+// let elly = JSON.parse(localStorage['donna']);
+//
+// elly.name = "Elly";
+// console.log(elly);
+
+
+
+
+
+
+
+
+
